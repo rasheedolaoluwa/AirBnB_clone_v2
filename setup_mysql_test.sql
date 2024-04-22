16a62fd2a2a48e984266a438d03520320f4908fd
rasheed <radeyola@gmail.com>
2024-04-22 03:22:08 -0700

    batch

---
+++ setup_mysql_test.sql
@@ -0,0 +1,12 @@
+-- Prepares MySQL server for project
+-- Creates project testing database with name : hbnb_test_db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
+-- with the password : hbnb_test_pwd if it dosen't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- granting the SELECT privilege
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
+-- granting all privileges to the new user on hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
